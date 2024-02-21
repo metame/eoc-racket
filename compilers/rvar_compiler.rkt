dbf2bded7a13d71cdb75502017df037c934ab44a
Michael J Erwin <michaeljameserwin@gmail.com>
2024-02-21 18:05:49 -0500

    Ch2: 2 passes of compiler finished

--- compilers/rvar_compiler.rkt
+++ compilers/rvar_compiler.rkt
@@ -1,48 +1,38 @@
 #lang racket
-
-(provide Int Prim Program pe-Rvar interp-Rvar)
-
-(require racket/dict)
+(require racket/set racket/stream)
 (require racket/fixnum)
+(require "interp-Lint.rkt")
+(require "interp-Lvar.rkt")
+(require "interp-Cvar.rkt")
+(require "type-check-Lvar.rkt")
+(require "type-check-Cvar.rkt")
+(require "utilities.rkt")
+(provide (all-defined-out))
 
-(require "public-student-support-code/utilities.rkt")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Lint examples
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; (struct Int (value))
-;; (struct Prim (op args))
-;; (struct Program (info body))
-;; (struct Var (var))
-;; (struct Let (var exp exp))
+;; The following compiler pass is just a silly one that doesn't change
+;; anything important, but is nevertheless an example of a pass. It
+;; flips the arguments of +. -Jeremy
+(define (flip-exp e)
+  (match e
+    [(Var x) e]
+    [(Prim 'read '()) (Prim 'read '())]
+    [(Prim '- (list e1)) (Prim '- (list (flip-exp e1)))]
+    [(Prim '+ (list e1 e2)) (Prim '+ (list (flip-exp e2) (flip-exp e1)))]))
 
-(define interp-Rvar-class
-  (class object%
-    (super-new)
+(define (flip-Lint e)
+  (match e
+    [(Program info e) (Program info (flip-exp e))]))
 
-    (define/public ((interp-exp env) e)
-      (match e
-        [(Int n) n]
-        [(Prim 'read '())
-         (define r (read))
-         (cond [(fixnum? r) r]
-               [else (error 'interp-exp "read expected an integer" r)])]
-        [(Prim '- (list e)) (fx- 0 ((interp-exp env) e))]
-        [(Prim '+ (list e1 e2))
-         (fx+ ((interp-exp env) e1) ((interp-exp env) e2))]
-        [(Var x) (dict-ref env x)]
-        [(Let x e body)
-         (define new-env (dict-set env x ((interp-exp env) e)))
-         ((interp-exp new-env) body)]))
 
-    (define/public (interp-program p)
-      (match p
-        [(Program '() e) ((interp-exp '()) e)]))))
-
-(define (interp-Rvar p)
-  (send (new interp-Rvar-class) interp-program p))
-
+;; Next we have the partial evaluation pass described in the book.
 (define (pe-neg r)
   (match r
     [(Int n) (Int (fx- 0 n))]
-    [else (Prim -' (list r))]))
+    [else (Prim '- (list r))]))
 
 (define (pe-add r1 r2)
   (match* (r1 r2)
@@ -54,10 +44,109 @@
     [(Int n) (Int n)]
     [(Prim 'read '()) (Prim 'read '())]
     [(Prim '- (list e1)) (pe-neg (pe-exp e1))]
-    [(Prim '+ (list e1 e2)) (pe-add (pe-exp e1) (pe-exp e2))]
-    [(Var x) (Var x)]
-    [(Let x e body) (Let x (pe-exp e) (pe-exp body))]))
+    [(Prim '+ (list e1 e2)) (pe-add (pe-exp e1) (pe-exp e2))]))
 
-(define (pe-Rvar p)
+(define (pe-Lint p)
   (match p
-    [(Program '() e) (Program '() (pe-exp e))]))
+    [(Program info e) (Program info (pe-exp e))]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; HW1 Passes
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (uniquify-exp env)
+  (lambda (e)
+    (match e
+      [(Var x)
+       (let ([new-sym (cadr (assoc x env))])
+         (Var new-sym))]
+      [(Int n) (Int n)]
+      [(Let x e body)
+       (let* ([new-sym (gensym)]
+              [new-env (cons (list x new-sym) env)])
+         (Let new-sym ((uniquify-exp env) e) ((uniquify-exp new-env) body)))]
+      [(Prim op es)
+       (Prim op (map (uniquify-exp env) es))])))
+
+;; uniquify : Lvar -> Lvar
+(define (uniquify p)
+  (match p
+    [(Program info e) (Program info ((uniquify-exp '()) e))]))
+
+;; returns new-sym, expression
+(define (rco-atom e)
+  (match e
+    [(Var x) (list '() (Var x))]
+    [(Int x) (list '() (Int x))]
+    [e
+     (let ([s (gensym)])
+       (list s (rco-exp e)))]
+    ;; [(Let x e body)
+    ;;  (let ([])
+    ;;    (list (rco-exp (Let x e body))))]
+    ;; [(Prim '- es) (Prim '- es)]
+    ;; [(Prim '+ es) (Prim '+ es)]
+    ;; [(Prim 'read '()) (Prim 'read '())]
+    ))
+
+(define (rco-exp e)
+  (match e
+    [(Var x) (Var x)]
+    [(Int x) (Int x)]
+    [(Let x e body) (Let x (rco-exp e) (rco-exp body))]
+    [(Prim '- (list e1))
+     (match (rco-atom e1)
+       [(list '() ae) (Prim '- (list ae))]
+       [(list s ae) (Let s ae (Prim '- (list (Var s))))])]
+    ;; fix prim + to use correct arg order
+    [(Prim '+ (list e1 e2))
+     (match (list (rco-atom e1) (rco-atom e2))
+       [(list (list '() ae1) (list '() ae2))
+        (Prim '+ (list ae1 ae2))]
+       [(list (list s1 ae1) (list '() ae2))
+        (Let s1 ae1 (Prim '+ (list (Var s1) ae2)))]
+       [(list (list '() ae1) (list s2 ae2))
+        (Let s2 ae2 (Prim '+ (list ae1 (Var s2))))]
+       [(list (list s1 ae1) (list s2 ae2))
+        (Let s1 ae1 (Let s2 ae2 (Prim '+ (list (Var s1) Var(s2)))))])]
+    [(Prim 'read '()) (Prim 'read '())]))
+
+;; remove-complex-opera* : Lvar -> Lvar^mon
+(define (remove-complex-opera* p)
+  (match p
+    [(Program info e) (Program info (rco-exp e))]))
+
+;; explicate-control : Lvar^mon -> Cvar
+(define (explicate-control p)
+  (error "TODO: code goes here (explicate-control)"))
+
+;; select-instructions : Cvar -> x86var
+(define (select-instructions p)
+  (error "TODO: code goes here (select-instructions)"))
+
+;; assign-homes : x86var -> x86var
+(define (assign-homes p)
+  (error "TODO: code goes here (assign-homes)"))
+
+;; patch-instructions : x86var -> x86int
+(define (patch-instructions p)
+  (error "TODO: code goes here (patch-instructions)"))
+
+;; prelude-and-conclusion : x86int -> x86int
+(define (prelude-and-conclusion p)
+  (error "TODO: code goes here (prelude-and-conclusion)"))
+
+;; Define the compiler passes to be used by interp-tests and the grader
+;; Note that your compiler file (the file that defines the passes)
+;; must be named "compiler.rkt"
+(define compiler-passes
+  `(
+     ;; Uncomment the following passes as you finish them.
+     ("uniquify" ,uniquify ,interp-Lvar ,type-check-Lvar)
+     ("remove complex opera*" ,remove-complex-opera* ,interp-Lvar ,type-check-Lvar)
+     ;; ("explicate control" ,explicate-control ,interp-Cvar ,type-check-Cvar)
+     ;; ("instruction selection" ,select-instructions ,interp-x86-0)
+     ;; ("assign homes" ,assign-homes ,interp-x86-0)
+     ;; ("patch instructions" ,patch-instructions ,interp-x86-0)
+     ;; ("prelude-and-conclusion" ,prelude-and-conclusion ,interp-x86-0)
+     ))
